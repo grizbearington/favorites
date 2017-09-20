ee5bb778d6ddb4fb4d57064ca38fe6fc5755d8dd
MCarter2 <carter34@umail.iu.edu>
2017-09-20 16:59:22 -0400

    functioning button
    added click listener
    favorite fills in and unfills.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+   cards = [
+       {
+         imgPath: "smg_fav.jpg",
+         title: "Super Mario Galaxy",
+         desc: "Best Game. Period!",
+         favorited: false
+       },
+       {
+           imgPath: "row_fav.jpg",
+           title: "Rowlet",
+           desc: "Best Owl. Period!",
+           favorited: false
+       },
+       {
+           imgPath: "Woah_JGMAITTP.jpg",
+           title: "Vegas Baby!",
+           desc: "Best Trip. Period",
+           favorited: false
+       }
+   ]
   constructor(public navCtrl: NavController) {
 
   }
 
+  clickFav(card) {
+    let index = this.cards.indexOf(card);
+    if(index > -1)
+      if (card.favorited){
+        this.cards[index].favorited = false;
+      }else{
+        this.cards[index].favorited = true;
+      }
+  }
 }
